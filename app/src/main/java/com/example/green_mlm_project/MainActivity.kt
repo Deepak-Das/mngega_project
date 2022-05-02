d16f3378cb2b700304f0e4b0f52aea64f211ceb3
Deepak <deepak42844@gmail.com>
2022-05-02 17:54:42 +0530

    login screen added validation also done

--- app/src/main/java/com/example/green_mlm_project/MainActivity.kt
+++ app/src/main/java/com/example/green_mlm_project/MainActivity.kt
@@ -8,8 +8,11 @@ import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.green_mlm_project.mngega_feature.presentaion.login.Login
 import com.example.green_mlm_project.mngega_feature.presentaion.ui.theme.Green_mlm_projectTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,7 +20,7 @@ class MainActivity : ComponentActivity() {
             Green_mlm_projectTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+                    Login()
                 }
             }
         }
@@ -26,7 +29,7 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+    Text(text = "Hello!!!!! $name!")
 }
 
 @Preview(showBackground = true)
---
+++ app/src/main/java/com/example/green_mlm_project/mngega_feature/presentaion/login/Login.kt
@@ -0,0 +1,140 @@
+package com.example.green_mlm_project.mngega_feature.presentaion.login
+
+
+import android.content.Context
+import android.util.Patterns
+import android.widget.Toast
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Visibility
+import androidx.compose.material.icons.filled.VisibilityOff
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.green_mlm_project.mngega_feature.presentaion.ui.theme.Purple700
+
+
+fun toast(message: String, context: Context) {
+    Toast.makeText(context, message, Toast.LENGTH_SHORT).show()
+}
+
+@Composable
+fun Login(
+    context: Context = LocalContext.current
+) {
+
+    var password by remember { mutableStateOf("") }
+    var passwordVisible by remember { mutableStateOf(false) }
+    var passwordHasError by remember { mutableStateOf(false) }
+    var passwordLabel by remember { mutableStateOf("Enter your password") }
+
+    var email by remember { mutableStateOf("") }
+    var emailHasError by remember { mutableStateOf(false) }
+    var emailLabel by remember { mutableStateOf("Enter your email address") }
+    val materialBlue700 = Purple700
+
+    if(email.isBlank()){
+        emailHasError=false
+        emailLabel="Enter your email address"
+    }
+    if(password.isNotBlank()){
+        passwordHasError=false
+        passwordLabel="Enter your password"
+    }
+//    val scaffoldState = rememberScaffoldState(rememberDrawerState(DrawerValue.Closed))
+    Scaffold(
+//        scaffoldState = scaffoldState,
+        topBar = {
+            TopAppBar(title = { Text("Login") }, actions = {
+                Row {
+                    Text(text = "Register")
+                    Icon(
+                        imageVector = Icons.Default.Add,
+                        contentDescription = "Register"
+                    )
+                }
+            }, backgroundColor = materialBlue700)
+        },
+//        floatingActionButtonPosition = FabPosition.End,
+//        floatingActionButton = { FloatingActionButton(onClick = {}){
+//            Text("X")
+//        } },
+//        drawerContent = { Text(text = "drawerContent") },
+        content = {
+
+            Column(
+                Modifier.fillMaxSize(1f),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+
+                OutlinedTextField(
+                    value = email,
+                    isError = emailHasError,
+                    label = { Text(text = emailLabel) },
+                    modifier = Modifier.padding(10.dp),
+                    onValueChange = { value -> email = value },
+                    keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Email),
+                )
+                OutlinedTextField(
+                    value = password,
+                    isError = passwordHasError,
+                    visualTransformation = if (passwordVisible) VisualTransformation.None else PasswordVisualTransformation(),
+                    label = { Text(text = passwordLabel) },
+                    modifier = Modifier.padding(10.dp),
+                    onValueChange = { value -> password = value },
+                    trailingIcon = {
+                        val image = if (passwordVisible)
+                            Icons.Filled.Visibility
+                        else Icons.Filled.VisibilityOff
+
+                        // Please provide localized description for accessibility services
+                        val description = if (passwordVisible) "Hide password" else "Show password"
+
+                        IconButton(onClick = { passwordVisible = !passwordVisible }) {
+                            Icon(imageVector = image, description)
+                        }
+                    }
+                )
+                Button(onClick = {
+                    when {
+                        password.isEmpty() -> {
+                            passwordHasError = true
+                            passwordLabel = "password can't be empty"
+                        }
+                        !Patterns.EMAIL_ADDRESS.matcher(email).matches() -> {
+                            emailHasError = true
+                            emailLabel = "Invalid email address"
+                        }
+                        else -> toast(message = "All fields are valid!", context)
+                    }
+                }) {
+                    Text("login")
+                }
+            }
+        },
+//        bottomBar = { BottomAppBar(backgroundColor = materialBlue700) { Text("BottomAppBar") } }
+    )
+
+
+}
+
+//fun Context.toast() {
+//    Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+//}
+
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    Login()
+}
